c896378a6b9db7130f954f4a7a3bb9d8b15b4ae4
shafikulnayem10 <inayem98@gmail.com>
2025-12-26 23:22:19 +0600

    modify file structure Of Cloud and Circle

--- openglportable/main.cpp
+++ openglportable/main.cpp
@@ -1,4 +1,61 @@
 #include <GL/glut.h>
+#include "Cloud.h"
+
+float coord_x = 0.0f;
+float coord_y = 0.0f;
+
+Cloud clouds[3];
+
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    glTranslatef(coord_x, coord_y, 0.0f);
+    drawClouds(clouds, 3);
+
+    glutSwapBuffers();
+}
+
+void KeyboardInput(unsigned char key, int x, int y)
+{
+    if (key == 'l')
+        coord_x += 0.05f;
+    if (key == 'r')
+        coord_x -= 0.05f;
+
+    glutPostRedisplay();
+}
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutInitWindowSize(2050, 1200);
+    glutCreateWindow("SkyFlyer - OpenGL OOP Project");
+
+    glClearColor(0.53f, 0.81f, 0.98f, 1.0f);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+
+    glutDisplayFunc(display);
+    glutKeyboardFunc(KeyboardInput);
+
+    clouds[0].x = -0.6f; clouds[0].y = 0.6f; clouds[0].speed = 0.0f;
+    clouds[1].x = -0.05f; clouds[1].y = 0.8f; clouds[1].speed = 0.0f;
+    clouds[2].x =  0.6f; clouds[2].y = 0.5f; clouds[2].speed = 0.0f;
+
+    glutMainLoop();
+    return 0;
+}
+
+
+
+
+/*#include <GL/glut.h>
 #include "Triangle.h"
 #include "Polygon.h"
 #include "drawLine.h"
@@ -64,7 +121,7 @@ int main(int argc, char** argv) {
     glutMainLoop();
 
     return 0;
-}
+}*/
 
 //copy from github
 /*
