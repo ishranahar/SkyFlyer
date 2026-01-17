bf3fe24814bc53ac1b81d52b915ed1cf7e835c3a
Esam10-user <mdesam99@gmail.com>
2026-01-17 12:43:18 +0600

    update to add interaction

---
+++ bird.h
@@ -0,0 +1,21 @@
+#ifndef BIRD_H
+#define BIRD_H
+
+class Bird {
+private:
+    float x, y;
+    float scale;
+    float wingAngle;
+
+public:
+    Bird(float xPos = 0.0f, float yPos = 0.0f, float s = 1.0f);
+
+    void update();
+    void draw();
+
+    void move(float dy);           
+    void moveHorizontal(float dx); 
+    void reset(float startX, float startY);
+};
+
+#endif
